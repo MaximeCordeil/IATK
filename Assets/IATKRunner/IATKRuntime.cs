5781e098191ad7b32fcfe188123ba9a79035440a
Philipp Fleck <philipp.fleck@icg.tugraz.at>
2021-04-30 15:09:24 +0200

    namespaces and cleanup

--- Assets/IATKRunner/IATKRuntime.cs
+++ Assets/IATKRunner/IATKRuntime.cs
@@ -7,245 +7,138 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IATKRuntime : MonoBehaviour
+namespace IATKExt
 {
-    // Start is called before the first frame update
-    int n;
-    float[] myValsX;
-    float[] myValsY;
-    float[] myValsZ;
-    float[] mySizes;
-    Color[] myColors;
-    View view = null;
-    ViewBuilder vb;
-    GameObject visGo = null;
-    Visualisation vis = null;
-    RealTimeDataSource rtds = null;
-
-    bool isVisReady = false;
-
-    void Start()
+    public class IATKRuntime : MonoBehaviour
     {
-        //CreateCodeBasedDummy();
-        StartCoroutine(DelayedInit());
-    }
+        // Start is called before the first frame update
+        int n;
+        float[] myValsX;
+        float[] myValsY;
+        float[] myValsZ;
+        float[] mySizes;
+        Color[] myColors;
+        View view = null;
+        ViewBuilder vb;
+        GameObject visGo = null;
+        Visualisation vis = null;
+        RealTimeDataSource rtds = null;
 
-    IEnumerator DelayedInit()
-    {
-        yield return new WaitForSeconds(0.5f);
-        CreateWithRTDataSource();
-    }
+        bool isVisReady = false;
 
-    void CreateWithRTDataSource()
-    {
-        //create DataSource
-        var rtdsGo = new GameObject("TestRTDS");
-        rtds = rtdsGo.AddComponent<RealTimeDataSource>();
-
-        //Add Dimension
-        rtds.AddDimension("DimA", 0, 100);
-        rtds.AddDimension("DimB", 0, 100);
-        rtds.AddDimension("DimC", 0, 100);
-        rtds.AddDimension("DimD", 0, 100);
-
-        rtds.AddDataByStr("DimA", 75f);
-        rtds.AddDataByStr("DimA", 50f);
-        rtds.AddDataByStr("DimA", 25f);
-
-        rtds.AddDataByStr("DimB", 25f);
-        rtds.AddDataByStr("DimB", 20f);
-        rtds.AddDataByStr("DimB", 25f);
-
-        StartCoroutine(SimulPoints());
-
-        //add source to graph
-        CreateVisFromSource();
-    }
-
-    Visualisation CreateVisFromSource()
-    {
-        visGo = new GameObject("myTester");
-        Debug.Log("Spawned myTester");
-
-        vis = visGo.AddComponent<Visualisation>();
-        Debug.Log("Add Visualizsation");
-
-        if (vis != null)
+        void Start()
         {
-
-            if(vis.theVisualizationObject == null)
-            {
-                vis.CreateVisualisation(AbstractVisualisation.VisualisationTypes.SCATTERPLOT);
-            }
-
-            vis.dataSource = rtds;
-            vis.xDimension = "DimA";
-            vis.yDimension = "DimB";
-            vis.zDimension = "DimC";
-            vis.sizeDimension = "DimD";
-            vis.CreateVisualisation(AbstractVisualisation.VisualisationTypes.SCATTERPLOT);
-
-
-            AbstractVisualisation abstractVisualisation = vis.theVisualizationObject;
-            
-            // Axis
-            abstractVisualisation.visualisationReference.xDimension.Attribute = "DimA";
-            abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.X);
-            abstractVisualisation.visualisationReference.yDimension.Attribute = "DimB";
-            abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.Y);
-            abstractVisualisation.visualisationReference.zDimension.Attribute = "DimC";
-            abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.Z);
-            abstractVisualisation.visualisationReference.sizeDimension = "DimD";
-            abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.OriginDimension);
-
-            vis.geometry = AbstractVisualisation.GeometryType.Bars;
-                        
-            Debug.Log("Init vis 6");
-            isVisReady = true;
+            //CreateCodeBasedDummy();
+            StartCoroutine(DelayedInit());
         }
-        return vis;
-    }
 
-    IEnumerator SimulPoints()
-    {
-        yield return new WaitForSeconds(5f);
-        //view = visGo.GetComponentInChildren<View>();
-        while (true)
+        IEnumerator DelayedInit()
         {
-            yield return new WaitForSeconds(0.01f);
-            try
+            yield return new WaitForSeconds(0.5f);
+            CreateWithRTDataSource();
+        }
+
+        void CreateWithRTDataSource()
+        {
+            //create DataSource
+            var rtdsGo = new GameObject("TestRTDS");
+            rtds = rtdsGo.AddComponent<RealTimeDataSource>();
+
+            //Add Dimension
+            rtds.AddDimension("DimA", 0, 100);
+            rtds.AddDimension("DimB", 0, 100);
+            rtds.AddDimension("DimC", 0, 100);
+            rtds.AddDimension("DimD", 0, 100);
+
+            rtds.AddDataByStr("DimA", 75f);
+            rtds.AddDataByStr("DimA", 50f);
+            rtds.AddDataByStr("DimA", 25f);
+
+            rtds.AddDataByStr("DimB", 25f);
+            rtds.AddDataByStr("DimB", 20f);
+            rtds.AddDataByStr("DimB", 25f);
+
+            StartCoroutine(SimulPoints());
+
+            //add source to graph
+            CreateVisFromSource();
+        }
+
+        Visualisation CreateVisFromSource()
+        {
+            visGo = new GameObject("myTester");
+            Debug.Log("Spawned myTester");
+
+            vis = visGo.AddComponent<Visualisation>();
+            Debug.Log("Add Visualizsation");
+
+            if (vis != null)
             {
-                if (rtds)
+
+                if (vis.theVisualizationObject == null)
                 {
-                    rtds.AddDataByStr("DimA", UnityEngine.Random.value * 100f);
-                    rtds.AddDataByStr("DimB", UnityEngine.Random.value * 100f);
-                    rtds.AddDataByStr("DimC", UnityEngine.Random.value * 100f);
-                    rtds.AddDataByStr("DimD", UnityEngine.Random.value * 100f);
-                    if (isVisReady && vis != null)
+                    vis.CreateVisualisation(AbstractVisualisation.VisualisationTypes.SCATTERPLOT);
+                }
+
+                vis.dataSource = rtds;
+                vis.xDimension = "DimA";
+                vis.yDimension = "DimB";
+                vis.zDimension = "DimC";
+                vis.sizeDimension = "DimD";
+                vis.CreateVisualisation(AbstractVisualisation.VisualisationTypes.SCATTERPLOT);
+
+
+                AbstractVisualisation abstractVisualisation = vis.theVisualizationObject;
+
+                // Axis
+                abstractVisualisation.visualisationReference.xDimension.Attribute = "DimA";
+                abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.X);
+                abstractVisualisation.visualisationReference.yDimension.Attribute = "DimB";
+                abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.Y);
+                abstractVisualisation.visualisationReference.zDimension.Attribute = "DimC";
+                abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.Z);
+                abstractVisualisation.visualisationReference.sizeDimension = "DimD";
+                abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.OriginDimension);
+
+                vis.geometry = AbstractVisualisation.GeometryType.Bars;
+
+                Debug.Log("Init vis 6");
+                isVisReady = true;
+            }
+            return vis;
+        }
+
+        IEnumerator SimulPoints()
+        {
+            yield return new WaitForSeconds(5f);
+            //view = visGo.GetComponentInChildren<View>();
+            while (true)
+            {
+                yield return new WaitForSeconds(0.01f);
+                try
+                {
+                    if (rtds)
                     {
-                        
-                        Debug.Log("-- SimulPoints before vis ...");
-                        //view.TweenPosition();
-                        //vb.updateView();
-                        vis.updateView(0);
-                        Debug.Log("-- SimulPoints after vis ...");
+                        rtds.AddDataByStr("DimA", UnityEngine.Random.value * 100f);
+                        rtds.AddDataByStr("DimB", UnityEngine.Random.value * 100f);
+                        rtds.AddDataByStr("DimC", UnityEngine.Random.value * 100f);
+                        rtds.AddDataByStr("DimD", UnityEngine.Random.value * 100f);
+                        if (isVisReady && vis != null)
+                        {
+
+                            Debug.Log("-- SimulPoints before vis ...");
+                            //view.TweenPosition();
+                            //vb.updateView();
+                            vis.updateView(0);
+                            Debug.Log("-- SimulPoints after vis ...");
+                        }
                     }
                 }
-            } catch(Exception err)
-            {
-                Debug.LogError("SimulPoints ERROR => " + err);
+                catch (Exception err)
+                {
+                    Debug.LogError("SimulPoints ERROR => " + err);
+                }
             }
         }
-    }
-
-    private void SetVisulisationParameters()
-    {
-        /*
-        if (IATKVisualisation.theVisualizationObject == null)
-        {
-            IATKVisualisation.CreateVisualisation(AbstractVisualisation.VisualisationTypes.SCATTERPLOT);
-        }
-
-        AbstractVisualisation abstractVisualisation = IATKVisualisation.theVisualizationObject;
-        VisualisationVariant parameters = visualisationVariant;
-
-        // Axis
-        abstractVisualisation.visualisationReference.xDimension.Attribute = parameters.xAxis;
-        abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.X);
-        abstractVisualisation.visualisationReference.yDimension.Attribute = parameters.yAxis;
-        abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.Y);
-        abstractVisualisation.visualisationReference.zDimension.Attribute = parameters.zAxis;
-        abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.Z);
-        */
-
-        /*
-        // Connectivity
-        if (parameters.geometryType == AbstractVisualisation.GeometryType.Lines ||
-            parameters.geometryType == AbstractVisualisation.GeometryType.LinesAndDots)
-        {
-            abstractVisualisation.visualisationReference.linkingDimension = parameters.linkingDimension;
-            abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.LinkingDimension);
-        }
-
-        // Set this to undefined to avoid problems
-        abstractVisualisation.visualisationReference.graphDimension = VisualisationVariant.undefined;
-        abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.GraphDimension);
-
-        // Aestethics
-        if (parameters.linkingDimension != VisualisationVariant.undefined) // TODO also check for GraphDimension
-        {
-            abstractVisualisation.visualisationReference.geometry = parameters.geometryType;
-            abstractVisualisation.UpdateVisualisation(AbstractVisualisation.PropertyType.GeometryType);
-        }
-
-        // Turn off shadows
-        MeshRenderer[] meshRenderers = IATKVisualisation.theVisualizationObject.GetComponentsInChildren<MeshRenderer>();
-        foreach (MeshRenderer meshRenderer in meshRenderers)
-        {
-            meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-        }
-        */
-    }
-
-
-    /*
-    void CreateCodeBasedDummy()
-    {
-        n = 150;
-        myValsX = new float[n];
-        myValsY = new float[n];
-        myValsZ = new float[n];
-        mySizes = new float[n];
-        myColors = new Color[n];
-
-        for (var i = 0; i < n; i++)
-        {
-            //myValsX[i] = (float)(i * i) / (float)(i + 1);
-            myValsX[i] = (float)i / (float)n;
-            myValsY[i] = 1f / (float)(i + 1);
-            myValsZ[i] = 1f / (float)(i + 1); ;
-            myColors[i] = new Color((i * 2 + 1) % 255, (i * 3 * i / 2) % 255, ((i + 13) * (i + 2) * (i + 1)) % 255);
-            //myColors[i] = Color.red;
-            mySizes[i] = 1;
-        }
-
-        vb = new ViewBuilder(MeshTopology.Points, "TestVis").
-                         initialiseDataView(n).
-                         setDataDimension(myValsX, ViewBuilder.VIEW_DIMENSION.X).
-                         setDataDimension(myValsY, ViewBuilder.VIEW_DIMENSION.Y).
-                         setDataDimension(myValsZ, ViewBuilder.VIEW_DIMENSION.Z).
-                         setSize(mySizes).
-                         setColors(myColors);
-
-        // use the IATKUtil class to get the corresponding Material mt 
-        Material mt = IATKUtil.GetMaterialFromTopology(AbstractVisualisation.GeometryType.Points);
-        mt.SetFloat("_MinSize", 0.01f);
-        mt.SetFloat("_MaxSize", 0.05f); //0.05
-
-
-        //vb.updateView();
-        visGo = new GameObject("myTester");
-        vis = visGo.AddComponent<Visualisation>();
-        vis.CreateVisualisation(AbstractVisualisation.VisualisationTypes.SCATTERPLOT);
-
-        var xAxisGo = new GameObject("xAxis");
-        xAxisGo.transform.parent = visGo.transform;
-        var xAxis = xAxisGo.AddComponent<IATK.Axis>();
-
-        view = vb.updateView().apply(visGo, mt);
-    }
-    */
-
-    // Update is called once per frame
-    void Update()
-    {
-        /*
-        for (var i = 0; i < n; i++)
-        {
-            myValsX[i] *= 1.0005f;
-        }
-        view.UpdateXPositions(myValsX);
-        vb.updateView();
-        */
     }
 }
--- Assets/IATKRunner/RealTimeDataSource.cs
+++ Assets/IATKRunner/RealTimeDataSource.cs
@@ -7,202 +7,205 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RealTimeDataSource : DataSource
+namespace IATKExt
 {
-    private int dimensionSizeLimit = 100;
-    private int dataCount;
-
-    private List<DimensionData> dimensionData = new List<DimensionData>();
-    private List<float[]> dimenstionDataArrays = new List<float[]>();
-    private List<int> lastIndices = new List<int>();
-
-    //do we need that
-    private Dictionary<string, Dictionary<int, string>> textualDimensionsList =
-        new Dictionary<string, Dictionary<int, string>>();
-
-
-    private float[] GetDefaultArray()
+    public class RealTimeDataSource : DataSource
     {
-        var dataArray = new float[dimensionSizeLimit];
-        for (int i = 0; i < dimensionSizeLimit; i++)
+        private int dimensionSizeLimit = 100;
+        private int dataCount;
+
+        private List<DimensionData> dimensionData = new List<DimensionData>();
+        private List<float[]> dimenstionDataArrays = new List<float[]>();
+        private List<int> lastIndices = new List<int>();
+
+        //do we need that
+        private Dictionary<string, Dictionary<int, string>> textualDimensionsList =
+            new Dictionary<string, Dictionary<int, string>>();
+
+
+        private float[] GetDefaultArray()
         {
-            dataArray[i] = 0;
-        }
-        return dataArray;
-    }
-
-    public void AddDimension(string dimensionName, float minVal, float maxVal)
-    {
-        var dataArray = GetDefaultArray();
-        dimenstionDataArrays.Add(dataArray);
-        lastIndices.Add(0);
-
-        //what are categories?
-        var metaData = new DimensionData.Metadata();
-        metaData.minValue = minVal;
-        metaData.maxValue = maxVal;
-        metaData.type = DataType.Float; //maybe make that adjustable
-        int newIndex = dimensionData.Count;
-
-        var dd = new DimensionData(dimensionName, newIndex, metaData);
-        dd.setData(dataArray, textualDimensionsList);
-        dimensionData.Add(dd);
-
-        dataCount += dimensionSizeLimit;
-    }
-
-
-    //This important for extern bindinds which might not support
-    //operator overloading or runtime reflection resolution
-    public void AddDataByIdnx(int index, float val)
-    {
-        if (index < dimensionData.Count)
-        {
-            var nextIndex = GetNextIndexForDimensionAndInc(index);
-            if (nextIndex >= 0)
+            var dataArray = new float[dimensionSizeLimit];
+            for (int i = 0; i < dimensionSizeLimit; i++)
             {
-                var dd = dimensionData[index];
-                dd.Data[nextIndex] = normaliseValue(val, dd.MetaData.minValue, dd.MetaData.maxValue, 0f, 1f);
+                dataArray[i] = 0;
             }
+            return dataArray;
         }
-    }
 
-    private int GetNextIndexForDimensionAndInc(int index)
-    {
-        if (index < lastIndices.Count)
+        public void AddDimension(string dimensionName, float minVal, float maxVal)
         {
-            return 0;
-            var ret = lastIndices[index];
-            lastIndices[index] = (lastIndices[index] + 1) % dimensionSizeLimit;
-            return ret;
+            var dataArray = GetDefaultArray();
+            dimenstionDataArrays.Add(dataArray);
+            lastIndices.Add(0);
+
+            //what are categories?
+            var metaData = new DimensionData.Metadata();
+            metaData.minValue = minVal;
+            metaData.maxValue = maxVal;
+            metaData.type = DataType.Float; //maybe make that adjustable
+            int newIndex = dimensionData.Count;
+
+            var dd = new DimensionData(dimensionName, newIndex, metaData);
+            dd.setData(dataArray, textualDimensionsList);
+            dimensionData.Add(dd);
+
+            dataCount += dimensionSizeLimit;
         }
-        return -1;
-    }
 
-    //This important for extern bindinds which might not support
-    //operator overloading or runtime reflection resolution
-    public void AddDataByStr(string dimensionName, float val)
-    {
-        var dd = this[dimensionName];
-        if (dd != null)
+
+        //This important for extern bindinds which might not support
+        //operator overloading or runtime reflection resolution
+        public void AddDataByIdnx(int index, float val)
         {
-            var index = dd.Index;
-            var nextIndex = GetNextIndexForDimensionAndInc(index);
-            if (nextIndex >= 0)
+            if (index < dimensionData.Count)
             {
-                for (var i = dimensionSizeLimit - 1; i >= 1; i--)
+                var nextIndex = GetNextIndexForDimensionAndInc(index);
+                if (nextIndex >= 0)
                 {
-                    dd.Data[i] = dd.Data[i - 1];
-                }
-
-                dd.Data[nextIndex] = normaliseValue(val, dd.MetaData.minValue, dd.MetaData.maxValue, 0f, 1f);
-            }
-        }
-    }
-
-    public override DimensionData this[int index]
-    {
-        get { return dimensionData[index]; }
-    }
-
-    public override DimensionData this[string identifier]
-    {
-        get
-        {
-            foreach (DimensionData d in dimensionData)
-            {
-                if (d.Identifier == identifier)
-                {
-                    return d;
+                    var dd = dimensionData[index];
+                    dd.Data[nextIndex] = normaliseValue(val, dd.MetaData.minValue, dd.MetaData.maxValue, 0f, 1f);
                 }
             }
-            return null;
         }
-    }
 
-    //can always be loaded since we fill it at runtime
-    public override bool IsLoaded
-    {
-        get { return true; }
-    }
+        private int GetNextIndexForDimensionAndInc(int index)
+        {
+            if (index < lastIndices.Count)
+            {
+                return 0;
+                var ret = lastIndices[index];
+                lastIndices[index] = (lastIndices[index] + 1) % dimensionSizeLimit;
+                return ret;
+            }
+            return -1;
+        }
 
-    public override int DimensionCount
-    {
-        get { return dimensionData.Count; }
-    }
+        //This important for extern bindinds which might not support
+        //operator overloading or runtime reflection resolution
+        public void AddDataByStr(string dimensionName, float val)
+        {
+            var dd = this[dimensionName];
+            if (dd != null)
+            {
+                var index = dd.Index;
+                var nextIndex = GetNextIndexForDimensionAndInc(index);
+                if (nextIndex >= 0)
+                {
+                    for (var i = dimensionSizeLimit - 1; i >= 1; i--)
+                    {
+                        dd.Data[i] = dd.Data[i - 1];
+                    }
 
-    public override int DataCount
-    {
-        get { return dataCount; }
-    }
+                    dd.Data[nextIndex] = normaliseValue(val, dd.MetaData.minValue, dd.MetaData.maxValue, 0f, 1f);
+                }
+            }
+        }
 
-    public override int getNumberOfCategories(int identifier)
-    {
-        throw new System.NotImplementedException();
-    }
+        public override DimensionData this[int index]
+        {
+            get { return dimensionData[index]; }
+        }
 
-    public override int getNumberOfCategories(string identifier)
-    {
-        throw new System.NotImplementedException();
-    }
+        public override DimensionData this[string identifier]
+        {
+            get
+            {
+                foreach (DimensionData d in dimensionData)
+                {
+                    if (d.Identifier == identifier)
+                    {
+                        return d;
+                    }
+                }
+                return null;
+            }
+        }
 
-    public override object getOriginalValue(float normalisedValue, string identifier)
-    {
-        DimensionData.Metadata meta = this[identifier].MetaData;
-        float normValue = normaliseValue(normalisedValue, 0f, 1f, meta.minValue, meta.maxValue);
-        return normValue;
-    }
+        //can always be loaded since we fill it at runtime
+        public override bool IsLoaded
+        {
+            get { return true; }
+        }
 
-    public override object getOriginalValue(float normalisedValue, int identifier)
-    {
-        DimensionData.Metadata meta = this[identifier].MetaData;
-        float normValue = normaliseValue(normalisedValue, 0f, 1f, meta.minValue, meta.maxValue);
-        return normValue;
-    }
+        public override int DimensionCount
+        {
+            get { return dimensionData.Count; }
+        }
 
-    //from CSVDataSource
-    float normaliseValue(float value, float i0, float i1, float j0, float j1)
-    {
-        float L = (j0 - j1) / (i0 - i1);
-        return (j0 - (L * i0) + (L * value));
-    }
+        public override int DataCount
+        {
+            get { return dataCount; }
+        }
 
-    public override object getOriginalValuePrecise(float normalisedValue, int identifier)
-    {
-        throw new System.NotImplementedException();
-    }
+        public override int getNumberOfCategories(int identifier)
+        {
+            throw new System.NotImplementedException();
+        }
 
-    public override object getOriginalValuePrecise(float normalisedValue, string identifier)
-    {
-        throw new System.NotImplementedException();
-    }
+        public override int getNumberOfCategories(string identifier)
+        {
+            throw new System.NotImplementedException();
+        }
 
-    public override void load()
-    {
-        //will be filled at runtime
-    }
+        public override object getOriginalValue(float normalisedValue, string identifier)
+        {
+            DimensionData.Metadata meta = this[identifier].MetaData;
+            float normValue = normaliseValue(normalisedValue, 0f, 1f, meta.minValue, meta.maxValue);
+            return normValue;
+        }
 
-    public override void loadHeader()
-    {
-        throw new System.NotImplementedException();
-    }
+        public override object getOriginalValue(float normalisedValue, int identifier)
+        {
+            DimensionData.Metadata meta = this[identifier].MetaData;
+            float normValue = normaliseValue(normalisedValue, 0f, 1f, meta.minValue, meta.maxValue);
+            return normValue;
+        }
 
-    private void Awake()
-    {
-        DataSourceManager.register(this);
-        if (!IsLoaded)
-            load();
-    }
+        //from CSVDataSource
+        float normaliseValue(float value, float i0, float i1, float j0, float j1)
+        {
+            float L = (j0 - j1) / (i0 - i1);
+            return (j0 - (L * i0) + (L * value));
+        }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+        public override object getOriginalValuePrecise(float normalisedValue, int identifier)
+        {
+            throw new System.NotImplementedException();
+        }
 
-    }
+        public override object getOriginalValuePrecise(float normalisedValue, string identifier)
+        {
+            throw new System.NotImplementedException();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        public override void load()
+        {
+            //will be filled at runtime
+        }
 
+        public override void loadHeader()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Awake()
+        {
+            DataSourceManager.register(this);
+            if (!IsLoaded)
+                load();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
